eb9bcae272639e267b4c9fe106c24749ae00f4ef
Kai <450507+neko-kai@users.noreply.github.com>
2025-02-17 15:33:49 +0000

    skip publish on PRs

--- devops/publish-scala.sh
+++ devops/publish-scala.sh
@@ -3,6 +3,8 @@ set -x
 
 cd json-sick-scala
 
+[[ "$CI_PULL_REQUEST" != "false"  ]] && exit 0
+
 if [[ "$CI_BRANCH_TAG" =~ ^v.*$ ]] ; then
     sbt +clean +test +publishSigned sonatypeBundleRelease
 else
