e8635b42b10f49e7e45553d63b9f977d0b7db9c3
Pavel Shirshov <pshirshov@eml.cc>
2025-05-13 18:28:00 +0100

    string table caches/interns its content

--- json-sick-csharp/SickSharp/Format/Tables/StringTable.cs
+++ json-sick-csharp/SickSharp/Format/Tables/StringTable.cs
@@ -1,4 +1,6 @@
+#nullable enable
 using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Text;
 
@@ -6,6 +8,8 @@ namespace SickSharp.Format.Tables
 {
     public class StringTable : VarTable<string>
     {
+        private readonly ConcurrentDictionary<int, WeakReference<string>> _cache = new();
+        
         public StringTable(Stream stream, int offset) : base(stream, offset)
         {
         }
@@ -13,6 +17,42 @@ namespace SickSharp.Format.Tables
         protected override string Convert(ReadOnlySpan<byte> bytes)
         {
             return Encoding.UTF8.GetString(bytes);
+        }
+
+        protected override string BasicRead(int absoluteStartOffset, int byteLen)
+        {
+            // While it's technically possible, we don't have overlapping packing, 
+            // so it's safe to use start offsets as keys
+            
+            // Convert(Stream.ReadSpan(absoluteStartOffset, byteLen))
+            string? ret;
+            
+            var r = _cache.AddOrUpdate(absoluteStartOffset, (i) =>
+            {
+                ret = base.BasicRead(i, byteLen);
+                return new WeakReference<string>(ret);
+            }, (i, reference) =>
+            {
+                if (reference.TryGetTarget(out _))
+                {
+                    return reference;
+                }
+
+                ret = base.BasicRead(i, byteLen);
+                return new WeakReference<string>(ret);
+            });
+            
+
+            if (r.TryGetTarget(out var retw))
+            {
+                return retw;
+            }
+
+            // in theory, this should never happen, because in both cases `ret` will retain the strong reference
+            ret = base.BasicRead(absoluteStartOffset, byteLen);
+            _cache[absoluteStartOffset] = new WeakReference<string>(ret);
+            
+            return ret;
         }
     }
 }
